ed26086eaecab33af7bc0c7d7ae3de8d909d9c8a
Caio "Quintas_" Santiago <caioquintassantiago@gmail.com>
2023-11-03 22:28:00 -0300

    add custom server

--- app/page.tsx
+++ app/page.tsx
@@ -11,82 +11,10 @@ const data = [
         phone: "123456789",
         updated_at: "22/12/22",
         created_at: "22/12/22",
-    }, {
-        id: 2,
-        pid: "oaijaaaaaaaaaaaaaaaaaaaasdoaisjdoasjd",
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-        phone: "123456789",
-        updated_at: "22/12/22",
-        created_at: "22/12/22",
-    }, {
-        id: 1,
-        pid: "oaijsdoaisjdoasjd",
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-        phone: "123456789",
-        updated_at: "22/12/22",
-        created_at: "22/12/22",
-    }, {
-        id: 1,
-        pid: "oaijsdoaisjdoasjd",
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-        phone: "123456789",
-        updated_at: "22/12/22",
-        created_at: "22/12/22",
-    }, {
-        id: 1,
-        pid: "oaijsdoaisjdoasjd",
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-        phone: "123456789",
-        updated_at: "22/12/22",
-        created_at: "22/12/22",
-    }, {
-        id: 1,
-        pid: "oaijsdoaisjdoasjd",
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-        phone: "123456789",
-        updated_at: "22/12/22",
-        created_at: "22/12/22",
-    }, {
-        id: 1,
-        pid: "oaijsdoaisjdoasjd",
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-        phone: "123456789",
-        updated_at: "22/12/22",
-        created_at: "22/12/22",
-    }, {
-        id: 1,
-        pid: "oaijsdoaisjdoasjd",
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-        phone: "123456789",
-        updated_at: "22/12/22",
-        created_at: "22/12/22",
-    }, {
-        id: 1,
-        pid: "oaijsdoaisjdoasjd",
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-        phone: "123456789",
-        updated_at: "22/12/22",
-        created_at: "22/12/22",
-    }, {
-        id: 1,
-        pid: "oaijsdoaisjdoasjd",
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-        phone: "123456789",
-        updated_at: "22/12/22",
-        created_at: "22/12/22",
     },
 ]
 
-export default async function Home() {
+export default function Home() {
     return <>
         <div className={"w-screen h-screen"}>
             <div className={"flex flex-col w-full h-full"}>
